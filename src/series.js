c12d62b7bcbe62bb2e23fcde84bc810e8ec61e79
rosswarren <rosswarren4@gmail.com>
2016-04-24 20:07:52 +0000

    changes to series function

--- src/series.js
+++ src/series.js
@@ -1,1 +1,1 @@
-module.exports = funcs => funcs.splice(1).reduce((acc, curr) => acc.then(curr()), funcs[0]());
+module.exports = funcs => funcs.splice(1).reduce((acc, curr) => acc.then(curr), funcs[0]());
--- test/series-test.js
+++ test/series-test.js
@@ -20,4 +20,14 @@ describe('series', () => {
             assert.deepEqual(results, ['a', 'b', 'c', 'd']);
         });
     });
+    
+    it('should pass the value of the previous to the current', () => {
+        const first = () => Promise.resolve(2);
+        const second = a => Promise.resolve(a + 6);
+        const third = a => Promise.resolve(a + 22);
+        
+        return series([first, second, third]).then(result => {
+            assert.equal(result, 30);
+        });
+    });
 });
