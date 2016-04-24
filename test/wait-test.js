249264a6bf2d533820e2498711f5208081370014
rosswarren <rosswarren4@gmail.com>
2016-04-24 18:03:55 +0000

    add test for wait

---
+++ test/wait-test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+
+const wait = require('../src/wait');
+
+describe('wait', () => {
+   it('should wait for the specified time then resolve', () => {
+       const time = Date.now();
+
+       return wait(5).then(() => {
+           const newTime = Date.now();
+           
+           assert(newTime - time >= 4); 
+           assert(newTime - time <= 6); 
+       });
+   });
+});
